16137cc47d929499e42ad10956b0becd6484d4c5
BeatrizMaresca <100882974+BeatrizMaresca@users.noreply.github.com>
2022-10-22 00:51:00 -0300

    Atualização 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import {Switch, Route, Link} from 'react-router-dom';
 import Home from './Home';
 import Cadastro from './Cadastro';
 import QuemSomos from './QuemSomos';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    var [numeroCliente, setNumeroCliente] = useState(0)
+    var [data, setData] = useState([]);
+
+    const getData=()=>{
+        fetch('http://localhost:5000/v2/listarCliente'
+        )
+        .then(function(response){
+            console.log(response)
+
+            setNumeroCliente(response.length)
+            setData(response);
+        })
+        .then(function(response) {
+            console.log('2')
+            setData(response)
+        });
+      }
+
+      return (
+        <div className="CallApi">
+            <div style={{backgroundColor:'green'}} className="display-board">
+                <h4 style={{color: 'white'}}>Criar Clientes</h4>
+                <div className="number">
+                    {numeroCliente}
+                </div>
+                <div className="btn">
+                    <button type="button" onClick={(e) => getData()} className="btn btn-warning">Listar clientes</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default function App() {
   return (
@@ -31,4 +67,3 @@ export default function App() {
     
   );
 }
-
